d602bb3c643a80917c8063d3827f97ba959fbd2c
karthik1896 <karthik1896@outlook.com>
2022-07-17 09:26:37 +0530

    light:Drop some flags which were not needed
    Signed-off-by: karthik1896 <karthik1896@outlook.com>

--- twrp_light.mk
+++ twrp_light.mk
@@ -6,16 +6,13 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Inherit from light device
 $(call inherit-product, device/xiaomi/light/device.mk)
 
 # Inherit some common Twrp stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := light
