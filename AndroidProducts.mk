5e7734a3db5533bb97600f7a263359f9b322d197
lesshiy <lesshiy300689@gmail.com>
2023-06-25 18:11:49 +0300

    Add files via upload

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,5 @@
 #
-# Copyright (C) 2022 The Android Open Source Project
-# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2022 TeamWin Recovery Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -9,6 +8,4 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_light.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_light-user \
-    twrp_light-userdebug \
     twrp_light-eng
--- twrp_light.mk
+++ twrp_light.mk
@@ -1,26 +1,22 @@
 #
-# Copyright (C) 2022 The Android Open Source Project
-# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2022 TeamWin Recovery Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/base.mk)
-
-# Virtual A/B
-ENABLE_VIRTUAL_AB := true
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from light device
 $(call inherit-product, device/xiaomi/light/device.mk)
 
-# Inherit some common Twrp stuff.
+# Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := light
 PRODUCT_NAME := twrp_light
-PRODUCT_BRAND := Redmi
-PRODUCT_MODEL := 22041219G
-PRODUCT_MANUFACTURER := xiaomi
+PRODUCT_BRAND := Poco
+PRODUCT_MODEL := Poco M4 5G
+PRODUCT_MANUFACTURER := Xiaomi
